8906a83792d768345c93fb255255224544436815
mrsdiouf660-svg <mrsdiouf660@gmail.com>
2026-02-26 13:16:15 +0000

    Update CV (1).py

--- CV (1).py
+++ CV (1).py
@@ -4,16 +4,16 @@ with st.sidebar:
     st.write("CONTACTS")
     st.write("Mrsdiouf660@gmail.com")
     st.write("Adresse:DakarPlateau")
-st.write("FORMATIONS")
-st.write("BTS EN GEOMATIQUE")
-st.subheader("PROJETS ACADEMIQUES")
+st.subheader("COMPETENCES")🎢
+st.write("géoréferencement")
+st.write("numerisation")
+st.write("maitrise des logiciels SIG:ARCGIS et QGIS") 
+st.subh("FORMATIONS")📓
+st.write("bts en géomatique")
+st.subheader("PROJETS ACADEMIQUES")📋
 st.write("Collecte de données")
 st.write("Création de base de données et Géoréférencement")
 st.write("Projet de collecte des Centre de Formations Professionnelles et Techniques")
-st.subheader("EXPERIENCES")
+st.subheader("EXPERIENCES")🪄
 st.write("Secrétaire Générale de la liste VERE(FLSH/UCAD)")
-st.subheader("COMPETENCES")
-st.write("géoréferencement")
-st.write("numerisation")
-st.write("maitrise des logiciels SIG:ARCGIS et QGIS")
 
